ce5abfa251380985c9e99b467b5c9641baa80c73
Ian Yeshua <130089514+YMast3r@users.noreply.github.com>
2024-09-05 00:34:32 -0600

    Subida del archivo Prueba.ino

---
+++ Prueba.ino
@@ -0,0 +1,28 @@
+int anguloI;
+
+void direccion(){
+    inclinacion();
+    if (-35 && -10){
+        anguloI = 1;
+    } else if (25 && 40){
+        anguloI = 2;
+    }
+    else {
+        angulo = 0;
+    }
+}
+void acciones(){
+if ( "CE" && "INT"){
+    Moverse(3,2);
+    if (anguloI == 1 || anguloI == 2){
+        delay(700);
+    }
+}
+}
+
+
+
+
+void inclinacion(){
+
+}
